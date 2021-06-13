25d5f05842af1fe9139f1faf785cc036cae6993d
aamingem <gem.aamin@gmail.com>
2021-06-13 10:46:47 +0200

    Fixed broken resources

--- src/tum/space/invaders/controller/music/BackgroundMusic.java
+++ src/tum/space/invaders/controller/music/BackgroundMusic.java
@@ -2,7 +2,7 @@ package tum.space.invaders.controller.music;
 
 public class BackgroundMusic extends Music {
 
-    private static final String BACKGROUND_MUSIC = "Rick_Roll.wav";
+    private static final String BACKGROUND_MUSIC = "background.mp3";
 
     public BackgroundMusic() {
         super(BACKGROUND_MUSIC);
