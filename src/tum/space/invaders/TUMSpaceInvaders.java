039c1b3ddf825e160731551263c9645910a1edfd
aamingem <gem.aamin@gmail.com>
2021-07-14 15:59:52 +0200

    Added a useful message

--- src/tum/space/invaders/TUMSpaceInvaders.java
+++ src/tum/space/invaders/TUMSpaceInvaders.java
@@ -4,22 +4,25 @@ public final class TUMSpaceInvaders {
 
     /**
      * Hey Can,
-     * We've implemented the very basics of stuff need to get this game to the very basic of running
-     * These are the FRs we've implemented in this task
-     * FR1: 1) The player should be able to shoot and move the spaceship
-     * FR2: 3) The score of the player should be displayed
-     * FR3: 4) Shooting sound must be played for every shot fired
-     * FR4: 5) The player should be able to start and stop the game
-     * FR5: 6) A background music should be played during the entire game
+     * We've implemented everything that we had to. The entire game works
+     * Both single player and multi player.
+     * For some reason there is a bug, which doesn't let the game be restarted within, but instead the app has to be relaunched
      *
-     * To move the spaceship, the player has to presses either A/Left key or D/Right key to go in the respective directions (no implemented yet)
-     * To shoot a laser beam, the player has to press K
+     * Other than that there are basic JavaFX bugs, i.e
+     * 1) If a laser beam is close enough to a spaceship, it counts as a hit, even if it is not touching
+     * 2) You can only register 1 keystroke at a time, instead of 2 keystrokes
+     * 3) It just crashes sometimes lol
      *
-     * Apart from that this game is also very immersive since it runs on the entire display (Minimum supported is 1280x720, max supported is 3840z2160)
-     * We don't believe that anyone on a 5K monitor would be playing this game
+     * Here are the controls:
+     * For player 1:
+     * A -> move left
+     * D -> move right
+     * W -> shoot
      *
-     * Apart from that we've implemented the Observer pattern here to update the location of the cars every frame
-     * effectively converting it to our own advantage, the UML diagram for it is at T03E02
+     * For player 2:
+     * J -> move left
+     * L -> move right
+     * I -> shoot
      */
 
     private TUMSpaceInvaders() {
