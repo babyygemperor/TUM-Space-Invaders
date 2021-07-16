e2f90c01e3dbc2215fdf139b2277994c5348de46
aamingem <gem.aamin@gmail.com>
2021-07-16 22:46:19 +0200

    Updated docs

--- src/tum/space/invaders/TUMSpaceInvaders.java
+++ src/tum/space/invaders/TUMSpaceInvaders.java
@@ -9,9 +9,9 @@ public final class TUMSpaceInvaders {
      * For some reason there is a bug, which doesn't let the game be restarted within, but instead the app has to be relaunched
      *
      * Other than that there are basic JavaFX bugs, i.e
-     * 1) If a laser beam is close enough to a spaceship, it counts as a hit, even if it is not touching
+     * 1) If a laser beam is close enough to a spaceship, it counts as a hit, even if it is not touching (Same bug as from Bumpers)
      * 2) You can only register 1 keystroke at a time, instead of 2 keystrokes
-     * 3) It just crashes sometimes lol
+     * EDIT: FIXED 0) It just crashes sometimes lol (Edit: We fixed that bug as well)
      *
      * Here are the controls:
      * For player 1:
